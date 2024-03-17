a26a7f255b150c4ab81c5b6efa02a15eb82afec9
Arkoprabho Das <87149382+ArkoprabhoDas@users.noreply.github.com>
2024-03-17 14:42:15 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-facebook_username = "Arkoprabho Das"
-facebook_password = "Arko1234#"
-pinterest_email = "arkoprabho900@gmail.com"
-pinterest_password = "Arko1234#"
-uri = "mongodb+srv://arkoprabho12:Arko1234#@arko0.r2kihyv.mongodb.net/ArkoDB?retryWrites=true&w=majority"
-database_name = "ArkoDB"
-collection_name = "Users"
